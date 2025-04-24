ea4ad16737b2be4af8101c1d01985282e4f40376
Pankaj Bhatia <panbhati@adobe.com>
2025-04-24 16:10:28 +0530

    implementing stats function

--- statistics.mjs
+++ statistics.mjs
@@ -1,5 +1,13 @@
 
 export function computeStatistics(numbers){
     
-    // implement the computation of statistics here
+   let average 
+   let total = numbers.reduce((acc,curr) =>(acc+curr),0)
+   average = total/numbers.length
+   console.log(average)
+   let min = Math.min(...numbers)
+   let max = Math.max(...numbers)
+
+   return {"average": average, "min": min, "max":  max}
 }
+
